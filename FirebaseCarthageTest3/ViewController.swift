bb675655e6462dea3f08f769191d078d7be76a91
Nicolai <niha@nodes.dk>
2018-08-27 12:33:29 +0200

    Initial Commit

---
+++ FirebaseCarthageTest3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseCarthageTest3
+//
+//  Created by Nicolai Harbo on 27/08/2018.
+//  Copyright © 2018 Nicolai Harbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
