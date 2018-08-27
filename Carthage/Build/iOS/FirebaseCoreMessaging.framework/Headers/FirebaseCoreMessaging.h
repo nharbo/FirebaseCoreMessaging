583da6bd87dd23fa825cf7871f65924fb5c66c94
Nicolai <niha@nodes.dk>
2018-08-27 14:09:37 +0200

    name changed

---
+++ Carthage/Build/iOS/FirebaseCoreMessaging.framework/Headers/FirebaseCoreMessaging.h
@@ -0,0 +1,19 @@
+//
+//  FirebaseCoreMessaging.h
+//  FirebaseCoreMessaging
+//
+//  Created by Nicolai Harbo on 27/08/2018.
+//  Copyright © 2018 Nicolai Harbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirebaseCoreMessaging.
+FOUNDATION_EXPORT double FirebaseCoreMessagingVersionNumber;
+
+//! Project version string for FirebaseCoreMessaging.
+FOUNDATION_EXPORT const unsigned char FirebaseCoreMessagingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseCoreMessaging/PublicHeader.h>
+
+
